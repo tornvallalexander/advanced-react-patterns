5b28f92341e5538596869e29e25c6b8f57badf22
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-07-24 09:07:49 +0200

    feat: added core functionality for compound components (sharing implicit state to children)

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,29 +4,24 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  // 📜 https://reactjs.org/docs/react-api.html#reactchildren
-  // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  return <Switch on={on} onClick={toggle} />
+  return React.Children.map(children, child => {
+    // what would, kind of, make sense, is to be able to extend
+    // the react element props, like below:
+    // child.props.on = on
+    // child.props.toggle = toggle
+    // but this will result in error, since the object is not extensible
+    // what React wants us to instead is:
+    return React.cloneElement(child, { on, toggle })
+  })
 }
 
-// 🐨 Flesh out each of these components
-
-// Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
-
-// Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
-
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
+const ToggleOn = ({on, children}) => on ? children : null
+const ToggleOff = ({on, children}) => on ? null : children
+const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
 
 function App() {
   return (
